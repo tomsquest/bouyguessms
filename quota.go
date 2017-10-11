b16a19936d93245e7fc8dcff494aeb2c5c96134e
Thomas Queste <tom@tomsquest.com>
2017-10-11 18:10:55 +0200

    doc: add doc to quota

--- quota.go
+++ quota.go
@@ -1,13 +1,17 @@
 package bouyguessms
 
+// Quota is the amount of remaining SMS
 type Quota int
 
+// ExceededQuota is the quota when there is no remaining SMS
 const ExceededQuota = Quota(0)
 
+// IsExceeded returns true when there is no remaining SMS
 func (quota Quota) IsExceeded() bool {
 	return int(quota) <= 0
 }
 
+// Remaining returns the amount of remaining SMS as an integer
 func (quota Quota) Remaining() int {
 	return int(quota)
 }
